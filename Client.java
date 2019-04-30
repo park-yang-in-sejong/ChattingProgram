905bb83c7224d230521b8cdf3be9914c10e67964
wngh0908 <38426656+wngh0908@users.noreply.github.com>
2019-04-30 11:58:32 +0900

    기본적인 채팅

---
+++ Client.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+	public static void main(String[] args) throws Exception {
+		//3. 서버소켓에 접속 시도
+//		Socket s = new Socket(String host,int port);
+		Socket s = new Socket("169.254.168.153", 5000);
+	}
+}
---
+++ Service.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Service extends Thread{ // 소켓 입출력 서비스
+	
+	Scanner in;
+	OutputStream out;
+	Vector<Service> v;
+	
+	public Service(Socket s, Vector<Service> v) {
+		this.v = v;
+		
+		try {
+			// 4.소켓 입출력 객체 생성
+			in = new Scanner(s.getInputStream());
+			out = s.getOutputStream();
+			start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+
+	//6. from 클라이언트 (클라이언트에서 전달한 메시지 읽기)
+	public void run() {
+		while(true) {
+			String msg = in.nextLine();
+			msg = msg+"\n";
+			toMsgAll(msg);
+		}
+	}
+	public void toMsgAll(String msg) { // 전체 접속 클라이언트에게 메시지 보내기
+		for (int i = 0; i < v.size(); i++) {// 접속한 클라이언트 수만큼 반복
+			try {
+				v.get(i).toMsg(msg);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void toMsg(String msg) throws IOException { // 실제 한개 한개의 클라이언트에게 메시지 보내기
+		//7. 서버 ---> 클라이언트
+		out.write(msg.getBytes());
+	}
+}
