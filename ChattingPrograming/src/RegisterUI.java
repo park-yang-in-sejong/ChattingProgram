8aa7ed230dc2f023ba9ada25d67fd759453a6a48
parksh <parksh5061@naver.com>
2019-05-24 20:24:15 +0900

    Complete login & register

---
+++ ChattingPrograming/src/RegisterUI.java
@@ -0,0 +1,124 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.*;
+import javax.swing.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import oracle.jdbc.driver.OracleDriver;
+
+public class RegisterUI extends JFrame{
+	
+	JPanel idPanel,passPanel,namePanel,loginPanel; 
+	JTextField userID,userName; // 유저 아이디, 유저 이름
+	JPasswordField userPasswd; // 유저 패스워드
+	JButton btnSubmit, btnCancel; // 가입 확인, 취소버튼
+	JLabel lb1,lb2,lb3,lb4;
+	String id,name,psw;
+	
+	public RegisterUI() {
+		this.setTitle("Register");
+		setLayout(new FlowLayout(FlowLayout.CENTER,0,30));
+		
+		lb1 = new JLabel("User Register");
+		lb1.setFont(new Font("맑은 고딕",Font.BOLD,25));
+		add(lb1);
+		
+		namePanel = new JPanel();
+		idPanel = new JPanel();
+		passPanel = new JPanel();
+		
+		lb2 = new JLabel("Name        ");
+		lb3 = new JLabel("ID               ");
+		lb4 = new JLabel("Password");
+		
+		userName = new JTextField(10);
+		userID = new JTextField(10);
+		userPasswd = new JPasswordField(10);
+		
+		namePanel.add(lb2);
+		namePanel.add(userName);
+		idPanel.add(lb3);
+		idPanel.add(userID);
+		passPanel.add(lb4);
+		passPanel.add(userPasswd);
+		
+		loginPanel = new JPanel();
+		btnSubmit = new JButton("Submit");
+		btnCancel = new JButton("Cancel");
+		
+		loginPanel.add(btnSubmit);
+		loginPanel.add(btnCancel);
+		add(namePanel);
+		add(idPanel);
+		add(passPanel);
+		add(loginPanel);
+		
+		setSize(300,400);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		btnSubmit.addActionListener(new MyAction());
+		btnCancel.addActionListener(new MyAction());
+	}
+	
+	class MyAction implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == btnSubmit){
+				Connection conn = null; // DB연결된 상태(세션)을 담은 객체
+		        PreparedStatement pstm = null;  // SQL 문을 나타내는 객체
+		        ResultSet rs = null;  // 쿼리문을 날린것에 대한 반환값을 담을 객체
+		        
+		        try {
+		            // SQL 문장을 만들고 만약 문장이 질의어(SELECT문)라면
+		            // 그 결과를 담을 ResulSet 객체를 준비한 후 실행시킨다.
+		            String query = "insert into chatuser values(?,?,?)";
+		            
+		            conn = DBConnection.getConnection();
+		            pstm = conn.prepareStatement(query);
+		           
+		            String username = userName.getText();		           
+		            String userid = userID.getText();
+		            String passwd = userPasswd.getText();
+		            		           
+		            pstm.setString(1, username);
+	                pstm.setString(2, userid);
+	                pstm.setString(3,passwd);
+	                
+	                pstm.executeUpdate();
+	                
+	                System.out.println("Complete to insert data");
+		            
+		        } catch (SQLException sqle) {
+		            sqle.printStackTrace();
+		            
+		        }finally{
+		            // DB 연결을 종료한다.
+		            try{
+		                if ( rs != null ){rs.close();}   
+		                if ( pstm != null ){pstm.close();}   
+		                if ( conn != null ){conn.close(); }
+		            }catch(Exception ex){
+		                throw new RuntimeException(ex.getMessage());
+		            }
+		        }
+			}
+			else if(e.getSource() == btnCancel) {
+				setVisible(false);
+				getContentPane().removeAll();
+				getContentPane().add(new MainUI());
+				revalidate();
+				repaint();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RegisterUI re = new RegisterUI();
+		
+	
+	}
+}
---
+++ ChattingPrograming/src/Server.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class Server extends Thread{
+
+	//1. 서버소켓객체 생성
+	ServerSocket ss;
+	
+	// 벡터는 어레이리스트랑 똑같음
+	Vector<Service> v; // 접속한 클라이언트 정보들을 저장, 관리
+	
+	public Server() {
+	
+		v = new Vector<Service>();
+	try {
+		//ss = new ServerSocket(포트);
+		ss = new ServerSocket(5000);
+		System.out.println("서버 시작...");
+		
+		start();
+		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	}// 생성자
+	
+	
+	public void run() {
+		try {
+			
+			while (true) {
+				//2. 생성을 했더라면, 클라이언트 접속 대기
+				//ss.accept(); // 서버 소켓이 클라이언트 기다림
+				Socket s = ss.accept(); // 들어온 클라이언트 받음
+				
+				Service serv = new Service(s,v); // 접속 클라이언트 수라 똑같다함
+				// 서비스는 소켓정보와 벡터정보도 같이 전달을 해야
+				
+				v.add(serv); // 벡터에다가 집어넣음!!
+				
+				InetAddress ia = s.getInetAddress();
+				System.out.println("클라이언트 접속 성공!!"+ ia.getHostAddress());
+			
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		new Server();
+	}
+
+}
